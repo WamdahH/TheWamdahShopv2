4165c3be24b97488b0ac6c6dd78361af215974be
Wamdah Hassan <wamdah.hassan@avanade.com>
2021-05-08 23:58:21 +0100

    Publish

---
+++ TheWamdahShop/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace TheWamdahShop.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> FindAll();
+        IEnumerable<T> FindByCondition(Expression<Func<T, bool>> expression);
+        T Create(T entity);
+        T Update(T entitiy);
+        void Delete(T entity);
+
+    }
+}
