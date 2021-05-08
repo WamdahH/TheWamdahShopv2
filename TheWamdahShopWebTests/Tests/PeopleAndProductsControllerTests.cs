4f21ad2668c4edbea94f08994ba1c129939dd275
Wamdah Hassan <wamdah.hassan@avanade.com>
2021-05-08 19:41:26 +0100

    Tests

---
+++ TheWamdahShopWebTests/Tests/PeopleAndProductsControllerTests.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+using TheWamdahShop.Controllers;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace TheWamdahShopWebTests.People
+{
+    public class PeopleAndProductsControllerTests
+    {
+
+        public class PeopleControllerTests { }
+        
+ 
+        //    private PeopleController peopleController;
+        //private Create createPerson;
+        //private Update updatePerson;
+        //private People course;
+        //private List<Person> courses;
+        //private Mock<ICourse> courseMock;
+        //private List<ICourse> coursesMock;
+        //private Mock<IAddCourse> addCourseMock;
+        //private Mock<IUpdateCourse> updateCourseMock;
+        //private Mock<ICourseViewModel> courseViewModelMock;
+        //private List<ICourseViewModel> coursesViewModelMock;
+        
+    };
+}
